ce4cc2efa6d5ce4ad9e5a4c9d424f37d9732c73b
Will <yzj_000@163.com>
2025-04-04 22:18:25 +0800

    Integration tests

---
+++ src/it/java/io/will/poc/kafka/HealthIT.java
@@ -0,0 +1,31 @@
+package io.will.poc.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = SpringKafkaPocApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-it.yml")
+public class HealthIT {
+    @Autowired
+    private MockMvc mvc;
+
+    // GET /health
+    @Test
+    public void testHealth() throws Exception {
+        ResultActions resultActions = mvc.perform(get("/health"));
+
+        resultActions.andExpect(status().isNoContent());
+    }
+}
