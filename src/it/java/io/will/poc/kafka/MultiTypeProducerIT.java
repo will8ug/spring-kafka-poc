775e4a526c34848671272faec34803819e643250
Will <yzj_000@163.com>
2025-04-19 15:01:52 +0800

    Introduce a custom annotation in IT

--- src/it/java/io/will/poc/kafka/MultiTypeProducerIT.java
+++ src/it/java/io/will/poc/kafka/MultiTypeProducerIT.java
@@ -1,19 +1,13 @@
 package io.will.poc.kafka;
 
-import io.will.poc.kafka.consumer.MultiTypeConsumer;
+import io.will.poc.kafka.annotation.KafkaMultiTypeTest;
 import io.will.poc.kafka.domain.Message;
 import io.will.poc.kafka.helper.CommonUtils;
-import io.will.poc.kafka.producer.MultiTypeProducer;
-import io.will.poc.kafka.resource.MultiTypeProducerResource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -27,11 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@ContextConfiguration(classes = {MultiTypeConsumer.class, MultiTypeProducer.class, MultiTypeProducerResource.class})
-@EnableAutoConfiguration
-@ComponentScan(value = {"io.will.poc.kafka.config", "io.will.poc.kafka.domain", "io.will.poc.kafka.helper"})
+@KafkaMultiTypeTest
 @AutoConfigureMockMvc
-@ActiveProfiles("simple")
 public class MultiTypeProducerIT {
     @Autowired
     private MockMvc mvc;
---
+++ src/it/java/io/will/poc/kafka/annotation/KafkaMultiTypeTest.java
@@ -0,0 +1,22 @@
+package io.will.poc.kafka.annotation;
+
+import io.will.poc.kafka.consumer.MultiTypeConsumer;
+import io.will.poc.kafka.producer.MultiTypeProducer;
+import io.will.poc.kafka.resource.MultiTypeProducerResource;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@ContextConfiguration(classes = {MultiTypeConsumer.class, MultiTypeProducer.class, MultiTypeProducerResource.class})
+@EnableAutoConfiguration
+@ComponentScan(value = {"io.will.poc.kafka.config", "io.will.poc.kafka.domain", "io.will.poc.kafka.helper"})
+@ActiveProfiles("simple")
+public @interface KafkaMultiTypeTest {
+}
