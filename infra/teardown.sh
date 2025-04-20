0e01709defa1a031ee017c8ab830cb64dc053b3d
Will <yzj_000@163.com>
2025-04-20 18:53:28 +0800

    Add the ability of skipping tearing down the infra containers

--- infra/teardown.sh
+++ infra/teardown.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+while [ $# -ge 1 ]; do
+  if [ "$1"x = "--skip-teardown"x ]; then
+    echo "'--skip-teardown' is found. Skipping teardown..."
+    exit
+  fi
+
+  shift
+done
+
 type nerdctl
 result=$?
 [ $result -ne 0 ] && {
